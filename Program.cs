7755a1b648b9ccc562d28bd07f9f195832217e64
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-04-07 15:33:44 +0530

    Ganesh Adding Greeting Message to Employee Pay Roll

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmpPayRollUsingThread
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Payroll problem using thread");
+        }
+    }
+}
