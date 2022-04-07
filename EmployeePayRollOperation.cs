d73b2b46a696645803ff0dcea430839c4c4b84b5
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-04-07 15:59:34 +0530

    Ganesh Adding file to Employee Pay Roll

---
+++ EmployeePayRollOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpPayRollUsingThread
+{
+    public class EmployeePayRollOperation
+    {
+        List<EmployeeDetails> employeedetails = new List<EmployeeDetails>();
+        public void AddEmployeePayroll(List<EmployeeDetails> listemployeedetails)
+        {
+            listemployeedetails.ForEach(employeeData =>
+            {
+                Console.WriteLine(employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine(employeeData.EmployeeName);
+
+
+            });
+            Console.WriteLine(this.employeedetails.ToString());
+
+
+        }
+        private abstract void addEmployeePayroll(EmployeeDetails emp)
+        {
+            employeedetails.Add(emp);
+        }
+        public void AddEmployeePayrollwiththread(List<EmployeeDetails> listemployeedetails)
+        {
+            listemployeedetails.ForEach(employeedata =>
+            {
+                Task tread = new Task(() =>
+                {
+                    Console.WriteLine(employeedata.EmployeeName);
+                    this.AddEmployeePayroll(listemployeedetails);
+                    Console.WriteLine(employeedata.EmployeeName);
+
+                });
+                tread.Start();
+            });
+            Console.WriteLine(this.employeedetails.ToString());
+        }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 namespace EmpPayRollUsingThread
 {
     public class Program
@@ -6,6 +8,88 @@ namespace EmpPayRollUsingThread
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Payroll problem using thread");
+            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+            employeeDetails.Add(new EmployeeDetails(1, "Joy", "58745689", "Kolkata", "Engineer", 'M', 457258.00, 250.00, 350.00, 300.00, "Kolkata", "India")),
+            employeeDetails.Add(new EmployeeDetails(1, "Joy", "58745689", "Kolkata", "Engineer", 'M', 457258.00, 250.00, 350.00, 300.00, "Kolkata", "India")),
+            employeeDetails.Add(new EmployeeDetails(1, "Joy", "58745689", "Kolkata", "Engineer", 'M', 457258.00, 250.00, 350.00, 300.00, "Kolkata", "India")),
+            employeeDetails.Add(new EmployeeDetails(1, "Joy", "58745689", "Kolkata", "Engineer", 'M', 457258.00, 250.00, 350.00, 300.00, "Kolkata", "India")),
+            employeeDetails.Add(new EmployeeDetails(1, "Joy", "58745689", "Kolkata", "Engineer", 'M', 457258.00, 250.00, 350.00, 300.00, "Kolkata", "India")),
+            employeeDetails.Add(new EmployeeDetails(1, "Joy", "58745689", "Kolkata", "Engineer", 'M', 457258.00, 250.00, 350.00, 300.00, "Kolkata", "India")),
+            employeeDetails.Add(new EmployeeDetails(1, "Joy", "58745689", "Kolkata", "Engineer", 'M', 457258.00, 250.00, 350.00, 300.00, "Kolkata", "India"))
+
+            EmployeePayrollOperation employeePayrollOperation = new EmployeePayrollOperation();
+            employeePayrollOperation.addEmployeePayroll(employeeDetails);
+            employeePayrollOpreation.EmployeePalyRoll(employeeDeatails);
+
+            Parallel Threading
+
+
+            string[] words = CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Begin first task.....");
+                GetLongestWord(words);
+            },
+
+            () =>
+            {
+                Console.WriteLine("Begin second task....");
+                GetMostCommonWords(words);
+            },
+            () =>
+            {
+                Console.WriteLine("Begin third task....");
+                GetCountForWords(words, "sleep");
+            }
+        );
+
+            static string[] CreateWordArray(string uri)
+            {
+                Console.WriteLine($"Retrieving from {uri}");
+                string s = new System.Net.WebClient().DownloadString(uri);
+
+                return s.Split(
+                    new char[] { ' ', '\u000A', ',', '.', ';', ':', '-', '_', '/' },
+                    StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+        private static string GetLongestWord(string[] words)
+        {
+            var longestWord = (from w in words
+                               orderby w.Length descending
+                               select w).First();
+            Console.WriteLine($"Task 1 -- The LONGEST WORD is {longestWord}");
+            return longestWord;
+        }
+
+        private static void GetCountForWords(string[] words, string term)
+        {
+            var findwords = from word in words
+                            where word.ToUpper().Contains(term.ToUpper())
+                            select word;
+            Console.WriteLine($@"Task 3 ---- the word ""{term}"" occurs {findwords.Count()} times.");
+
+        }
+
+        private static void GetMostCommonWords(string[] words)
+        {
+            var frequencyorder = from word in words
+                                 where word.Length > 6
+                                 group word by word into g
+                                 orderby g.Count() descending
+                                 select g.Key;
+
+            var commanwords = frequencyorder.Take(10);
+            System.Text.StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Task 2 -- the most common words are :");
+            foreach (var v in commanwords)
+            {
+                sb.AppendLine(" " + v);
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
+    
